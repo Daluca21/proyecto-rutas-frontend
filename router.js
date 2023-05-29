79d588135348b925d562090900534ab3c2e455af
Luis Carreño <>
2023-05-29 14:50:02 -0500

    Cambios menores

--- router.js
+++ router.js
@@ -1,93 +1,92 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const session = require('express-session');
-const axios = require('axios');
+const session = require("express-session");
+const axios = require("axios");
 
-router.get('/contacto', (req,res)=>{
-    res.render('contacto');
-})
+router.get("/contacto", (req, res) => {
+  res.render("contacto");
+});
 
-router.get('/registro', (req,res)=>{
-    const mensaje = req.query.mensaje || null;
-    res.render('registro', {mensaje});
-})
+router.get("/registro", (req, res) => {
+  const mensaje = req.query.mensaje || null;
+  res.render("registro", { mensaje });
+});
 
-router.get('/login', (req,res)=>{
-    const mensaje = req.query.mensaje || null;
-    res.render('login', {mensaje});
-})
+router.get("/login", (req, res) => {
+  const mensaje = req.query.mensaje || null;
+  res.render("login", { mensaje });
+});
 
 function verificarAutenticacion(req, res, next) {
-    if (req.session.loggedIn) {
-        // El usuario está autenticado, continuar con la solicitud
-        next();
-    } else {
-        // El usuario no está autenticado, redirigir al formulario de inicio de sesión
-        res.redirect('/login');
-    }
+  if (req.session.loggedIn) {
+    // El usuario está autenticado, continuar con la solicitud
+    next();
+  } else {
+    // El usuario no está autenticado, redirigir al formulario de inicio de sesión
+    res.redirect("/login");
+  }
 }
 
 const isAdmin = (req, res, next) => {
-    // Ejemplo de validación básica: Verificar si el usuario tiene un rol de administrador
-    const isAdminUser = req.session.loggedIn && req.session.admin;
-    if (isAdminUser) {
-      next();
-    } else {
-      // Si el usuario no es administrador, redirecciona a una página de error o muestra un mensaje de acceso denegado
-      res.redirect('/admin');
-    }
-  };
-  
+  // Ejemplo de validación básica: Verificar si el usuario tiene un rol de administrador
+  const isAdminUser = req.session.loggedIn && req.session.admin;
+  if (isAdminUser) {
+    next();
+  } else {
+    // Si el usuario no es administrador, redirecciona a una página de error o muestra un mensaje de acceso denegado
+    res.redirect("/admin");
+  }
+};
 
-router.get('/perfil', verificarAutenticacion, (req, res) => {
-    const user = req.session.user || null;
-    res.render('perfil', {usuario: user});
+router.get("/perfil", verificarAutenticacion, (req, res) => {
+  const user = req.session.user || null;
+  res.render("perfil", { usuario: user });
 });
 
-router.get('/', (req,res)=>{
+router.get("/", (req, res) => {
   const direccionRuta = "http://localhost:8080/ruta";
-  const loginOn = req.session.loggedIn || null
-  axios.get(direccionRuta + "/all") //Ajustar para obtener por empresa
-    .then(response => {
-        res.render('index', { rutas: response.data, loginOn });
+  const loginOn = req.session.loggedIn || null;
+  axios
+    .get(direccionRuta + "/all") //Ajustar para obtener por empresa
+    .then((response) => {
+      res.render("index", { rutas: response.data, loginOn });
     })
-    .catch(error => {
-      res.render('index', { rutas: null, loginOn });
-    })
-});
-
-router.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-      if (err) {
-        console.error('Error al cerrar sesión:', err);
-      }
-      res.redirect('/login');
+    .catch((error) => {
+      res.render("index", { rutas: null, loginOn });
     });
 });
 
-router.get('/admin', (req,res)=>{
-    const mensaje = req.query.mensaje || null;
-    res.render('login_admin', {mensaje});
-})
-
-router.get('/admin/add', isAdmin, (req,res)=>{
-    const mensaje = req.query.mensaje || null;
-    res.render('agregar_ruta', {mensaje});
-})
-
-router.get('/admin/dashboard', isAdmin, (req, res) => {
-    const direccionRuta = "http://localhost:8080/ruta";
-    const rutaData = req.body;
-    console.log(direccionRuta + "/all");
-    console.log(rutaData);
-    axios.get(direccionRuta + "/all") //Ajustar para obtener por empresa
-      .then(response => {
-          res.render('dashboard', { rutas: response.data });
-      })
-      .catch(error => {
-        res.render('dashboard', { rutas: null });
-      })
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error("Error al cerrar sesión:", err);
+    }
+    res.redirect("/login");
   });
-  
+});
+
+router.get("/admin", (req, res) => {
+  const mensaje = req.query.mensaje || null;
+  res.render("login_admin", { mensaje });
+});
+
+router.get("/admin/add", isAdmin, (req, res) => {
+  res.render("agregar_ruta");
+});
+
+router.get("/admin/dashboard", isAdmin, (req, res) => {
+  const direccionRuta = "http://localhost:8080/ruta";
+  const rutaData = req.body;
+  console.log(direccionRuta + "/all");
+  console.log(rutaData);
+  axios
+    .get(direccionRuta + "/all") //Ajustar para obtener por empresa
+    .then((response) => {
+      res.render("dashboard", { rutas: response.data });
+    })
+    .catch((error) => {
+      res.render("dashboard", { rutas: null });
+    });
+});
 
 module.exports = router;
--- routes/admin.js
+++ routes/admin.js
@@ -1,55 +1,56 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 const direccion = "http://localhost:8080/administrador"; //Url para realizar la consulta a la base de datos
 const direccionRuta = "http://localhost:8080/ruta"; //Url para realizar la consulta a la base de datos
-const session = require('express-session');
+const session = require("express-session");
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const userData = {
     correo: req.body.correo,
     hashPassword: req.body.password,
   };
   console.log(direccion + "/get");
-  axios.post(direccion + "/get", userData)
-    .then(response => {
-        if (response.data.success) {
-            // Las credenciales son válidas, iniciar sesión y redirigir al usuario a una página protegida
-            console.log(response.data);
-            req.session.loggedIn = true;
-            req.session.correo = response.data.data.correo;
-            req.session.id = response.data.data.id;
-            req.session.user = response.data.data;
-            req.session.admin = true; //Identifica si la sesion es de un admin
-            req.session.sadmin = response.data.data.superadmin;
-            console.log(response.data.data);
-            if(req.session.sadmin) res.redirect('/sadmin'); //Redirige al Dashboard
-            else res.redirect('/admin/dashboard');
-        } else {
-            res.render('login_admin', { mensaje: 'Credenciales inválidas' });
-        }
+  axios
+    .post(direccion + "/get", userData)
+    .then((response) => {
+      if (response.data.success) {
+        // Las credenciales son válidas, iniciar sesión y redirigir al usuario a una página protegida
+        console.log(response.data);
+        req.session.loggedIn = true;
+        req.session.correo = response.data.data.correo;
+        req.session.id = response.data.data.id;
+        req.session.user = response.data.data;
+        req.session.admin = true; //Identifica si la sesion es de un admin
+        req.session.sadmin = response.data.data.superadmin;
+        console.log(response.data.data);
+        if (req.session.sadmin) res.redirect("/sadmin"); //Redirige al Dashboard
+        else res.redirect("/admin/dashboard");
+      } else {
+        res.render("login_admin", { mensaje: "Credenciales inválidas" });
+      }
     })
-    .catch(error => {
-      res.render('login_admin', { mensaje: 'Hubo un error al iniciar sesión, verifique la informacion' });
-    })
+    .catch((error) => {
+      res.render("login_admin", {
+        mensaje: "Hubo un error al iniciar sesión, verifique la informacion",
+      });
+    });
 });
 
-router.post('/add', (req, res) => {
+router.post("/add", (req, res) => {
   const rutaData = req.body;
   console.log(direccionRuta + "/add");
   console.log(rutaData);
-  axios.post(direccionRuta + "/add", rutaData)
-    .then(response => {
-        if (response.data != 'Saved') {
-          
-        }
-        console.log(response.data);
-        //res.render('agregar_ruta', { mensaje: 'Hubo un error al guardar la ruta, verifique la informacion' });  
-        res.render('agregar_ruta', { mensaje: 'Ruta guardada exitosamente' });
+  axios
+    .post(direccionRuta + "/add", rutaData)
+    .then((response) => {
+      res.json({ message: "Ruta guardada exitosamente" });
     })
-    .catch(error => {
-      res.render('agregar_ruta', { mensaje: 'Hubo un error al guardar la ruta, verifique la informacion' });
-    })
+    .catch((error) => {
+      res.json({
+        message: "Hubo un error al guardar la ruta, verifique la información",
+      });
+    });
 });
 
 module.exports = router;
